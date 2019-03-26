90bd7577ed94cf03881d67869c2a9f40e1a92d45
huangzh <873556196@qq.com>
2019-03-26 22:50:39 +0800

    暂时先看这么多

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,13 @@
 import 'babel-polyfill';
 import './class/lesson1';
+// import { A, test, Hello } from './class/lesson17';
+// import { A } from './class/lesson17';
+// import * as lesson from './class/lesson17';
+
+import Lesson17 from './class/lesson17';
+
+// console.log(A, test, Hello);
+// console.log(A);
+// console.log(lesson.A, lesson.test);
+
+console.log(Lesson17.A);
